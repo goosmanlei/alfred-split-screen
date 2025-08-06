678b8fdd90c972c7ab43cb9670b5573cae49c7bc
leiguoguo <leiguoguo@bytedance.com>
2025-08-06 20:02:48 +0800

    upgrade

--- split-screen.awk
+++ split-screen.awk
@@ -20,6 +20,5 @@ END {
     split(substr(Origin["main"], 2, length(Origin["main"]) - 2), MainOrigin, ",");
     split(Resolution["dual"], DualResolution, "x");
     split(substr(Origin["dual"], 2, length(Origin["dual"]) - 2), DualOrigin, ",");
-    #printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d", MainOrigin[1], MainOrigin[2], MainResolution[1], MainResolution[2], DualOrigin[1], DualOrigin[2], DualResolution[1], DualResolution[2]);
     printf("{\n    \"main\": {\"x\": %d, \"y\": %d, \"width\": %d, \"height\": %d},\n    \"dual\": {\"x\": %d, \"y\": %d, \"width\": %d, \"height\": %d}\n}", MainOrigin[1], MainOrigin[2], MainResolution[1], MainResolution[2], DualOrigin[1], DualOrigin[2], DualResolution[1], DualResolution[2]);
 }
--- split-screen.sh
+++ split-screen.sh
@@ -2,8 +2,6 @@
 SCRIPT_PATH=$(dirname "$(realpath ${BASH_SOURCE[0]})")
 CSV_FNAME="${SCRIPT_PATH}/split-screen.csv"
 CSV_TMP_FNAME="${SCRIPT_PATH}/split-screen.csv.tmp"
-SCPT_FNAME="${SCRIPT_PATH}/split-screen.scpt"
-SCPT_TPL_FNAME="${SCRIPT_PATH}/split-screen.tpl"
 
 if [ "X$1" == "Xshow" ] ; then
 awk -v "showWhere=$2" -v "filterPattern=$3" '
@@ -47,42 +45,3 @@ END {
 }
 ' "${CSV_FNAME}" > "${CSV_TMP_FNAME}"
 mv "${SCRIPT_PATH}/split-screen.csv.tmp" "${SCRIPT_PATH}/split-screen.csv"
-
-awk '
-Begin {
-	NumberOfRecord = 0
-}
-NF == 5 && NR == FNR{
-	NumberOfRecord ++
-	Infos_name[NumberOfRecord] = $1;
-	Infos_x[NumberOfRecord] = $2;
-	Infos_y[NumberOfRecord] = $3;
-	Infos_w[NumberOfRecord] = $4;
-	Infos_h[NumberOfRecord] = $5;
-}
-NR > FNR {
-	if (!print_after_replace) {
-		if ($0 != "----TPL_REPLACE_WITH_CONFIG") {
-			print $0
-		} else {
-			print_after_replace = 1
-			for (Idx = 1; Idx <= NumberOfRecord; Idx ++) {
-				if (Idx == 1) {
-					printf("    if positionType as string is equal to \"%s\" then\n", Infos_name[Idx]);
-					printf("        set {percentX, percentY, percentW, percentH} to {%0.2f, %0.2f, %0.2f, %0.2f}\n", Infos_x[Idx], Infos_y[Idx], Infos_w[Idx], Infos_h[Idx]);
-				} else if (Idx < NumberOfRecord) {
-					printf("    else if positionType as string is equal to \"%s\" then\n", Infos_name[Idx]);
-					printf("        set {percentX, percentY, percentW, percentH} to {%0.2f, %0.2f, %0.2f, %0.2f}\n", Infos_x[Idx], Infos_y[Idx], Infos_w[Idx], Infos_h[Idx]);
-				} else {
-					printf("    else if positionType as string is equal to \"%s\" then\n", Infos_name[Idx]);
-					printf("        set {percentX, percentY, percentW, percentH} to {%0.2f, %0.2f, %0.2f, %0.2f}\n", Infos_x[Idx], Infos_y[Idx], Infos_w[Idx], Infos_h[Idx]);
-					printf("    end if\n");
-				}
-			}
-		}
-	} else if (print_after_replace) {
-		print $0
-	}
-}
-' "${CSV_FNAME}" "${SCPT_TPL_FNAME}" > "${SCPT_FNAME}"
-
